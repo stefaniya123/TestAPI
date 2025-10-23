38bf45e1f6d2e270ca203a5e475871c9a461cce7
Stefaniya K <foxzz@bk.ru>
2025-10-23 18:27:48 +0300

    push error2

--- test_api/test_api_ci.py
+++ test_api/test_api_ci.py
@@ -16,7 +16,7 @@ def api_session():
 def test_get_simple(base_url, api_session):
     """Тест: простой GET-запрос без параметров"""
     response = api_session.get(f"{base_url}/get")
-    assert response.status_code == 200
+    assert response.status_code == 200,  "Ждем код ответа сервера 200"
 
     data = response.json()
     assert data["url"] == f"{base_url}/get"
@@ -27,7 +27,7 @@ def test_get_with_query_params(base_url, api_session):
     """Тест: GET с query-параметрами"""
     params = {"test_key": "test_value", "number": "123"}
     response = api_session.get(f"{base_url}/get", params=params)
-    assert response.status_code == 200
+    assert response.status_code == 200,  "Ждем код ответа сервера 200"
 
     data = response.json()
     assert data["args"] == params
@@ -37,7 +37,7 @@ def test_post_json(base_url, api_session):
     """Тест: POST с JSON-телом"""
     payload = {"message": "Hello", "flag": True, "count": 100}
     response = api_session.post(f"{base_url}/post", json=payload)
-    assert response.status_code == 204
+    assert response.status_code == 204,  "Ждем код ответа сервера 200"
 
     data = response.json()
     assert data["json"] == payload
@@ -46,7 +46,7 @@ def test_post_form_data(base_url, api_session):
     """Тест: POST с form-encoded данными"""
     form_data = {"username": "pytest_user", "role": "tester"}
     response = api_session.post(f"{base_url}/post", data=form_data)
-    assert response.status_code == 200
+    assert response.status_code == 200,  "Ждем код ответа сервера 200"
 
     data = response.json()
     assert data["form"] == form_data
@@ -55,7 +55,7 @@ def test_custom_header_in_request(base_url,api_session):
     """Тест: запрос с кастомным заголовком"""
     headers = {"X-Test-Client": "pytest-automation"}
     response = api_session.post(f"{base_url}/post", json={}, headers=headers)
-    assert response.status_code == 200
+    assert response.status_code == 200,  "Ждем код ответа сервера 200"
 
     data = response.json()
     assert "x-test-client" in data["headers"]
